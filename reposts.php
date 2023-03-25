48d547a01cf8cea9ebf02b9859ebdc050db4aced
Ganesh <wakadeganesh6@gmail.com>
2023-03-25 20:21:18 +0530

    Set different Bar colour in Query Report

--- reposts.php
+++ reposts.php
@@ -19,8 +19,8 @@ $rows = mysqli_fetch_all($b, MYSQLI_ASSOC);
 
 // Loop through the rows and concatenate the values into a string
 $bcnt = "";
-foreach($rows as $row) {
-    $bcnt .= implode(",", $row) . "\n";
+foreach ($rows as $row) {
+  $bcnt .= implode(",", $row) . "\n";
 }
 
 // Output the string
@@ -37,120 +37,120 @@ $updatebusicnt = "UPDATE reports SET count=$bcnt WHERE id = 1 ";
 
 //   Investment Update 
 
-  $invcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('investment', query) > 0 ";
-  $inv = mysqli_query($conn, $invcount);
-  $irows = mysqli_fetch_all($inv, MYSQLI_ASSOC);
-  $icnt = "";
-foreach($irows as $row) {
-    $icnt .= implode(",", $row) . "\n";
+$invcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('investment', query) > 0 ";
+$inv = mysqli_query($conn, $invcount);
+$irows = mysqli_fetch_all($inv, MYSQLI_ASSOC);
+$icnt = "";
+foreach ($irows as $row) {
+  $icnt .= implode(",", $row) . "\n";
 }
 $updateicnt = "UPDATE reports SET count=$icnt WHERE id = 2 ";
 
 
 // Relationship Updation 
 
-  $relcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('relationship', query) > 0 ";
-  $rel = mysqli_query($conn, $relcount);
-  $relrows = mysqli_fetch_all($rel, MYSQLI_ASSOC);
-  $relcnt = "";
-foreach($relrows as $row) {
-    $relcnt .= implode(",", $row) . "\n";
+$relcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('relationship', query) > 0 ";
+$rel = mysqli_query($conn, $relcount);
+$relrows = mysqli_fetch_all($rel, MYSQLI_ASSOC);
+$relcnt = "";
+foreach ($relrows as $row) {
+  $relcnt .= implode(",", $row) . "\n";
 }
 $updaterelcnt = "UPDATE reports SET count=$relcnt WHERE id = 3 ";
 
 
 // Health Updation 
 
-  $hltcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('health', query) > 0 ";
-  $hlt = mysqli_query($conn, $hltcount);
-  $hltrows = mysqli_fetch_all($hlt, MYSQLI_ASSOC);
-  $hltcnt = "";
-foreach($hltrows as $row) {
-    $hltcnt .= implode(",", $row) . "\n";
+$hltcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('health', query) > 0 ";
+$hlt = mysqli_query($conn, $hltcount);
+$hltrows = mysqli_fetch_all($hlt, MYSQLI_ASSOC);
+$hltcnt = "";
+foreach ($hltrows as $row) {
+  $hltcnt .= implode(",", $row) . "\n";
 }
 $updatehltcnt = "UPDATE reports SET count=$hltcnt WHERE id = 4 ";
 
 
 // Property Updation 
 
-  $prtcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('property', query) > 0 ";
-  $prt = mysqli_query($conn, $prtcount);
-  $prtrows = mysqli_fetch_all($prt, MYSQLI_ASSOC);
-  $prtcnt = "";
-foreach($prtrows as $row) {
-    $prtcnt .= implode(",", $row) . "\n";
+$prtcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('property', query) > 0 ";
+$prt = mysqli_query($conn, $prtcount);
+$prtrows = mysqli_fetch_all($prt, MYSQLI_ASSOC);
+$prtcnt = "";
+foreach ($prtrows as $row) {
+  $prtcnt .= implode(",", $row) . "\n";
 }
 $updateprtcnt = "UPDATE reports SET count=$prtcnt WHERE id = 5 ";
 
 
 // Marriage Updation 
 
-  $mricount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('marriage', query) > 0 ";
-  $mri = mysqli_query($conn, $mricount);
-  $mrirows = mysqli_fetch_all($mri, MYSQLI_ASSOC);
-  $mricnt = "";
-foreach($mrirows as $row) {
-    $mricnt .= implode(",", $row) . "\n";
+$mricount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('marriage', query) > 0 ";
+$mri = mysqli_query($conn, $mricount);
+$mrirows = mysqli_fetch_all($mri, MYSQLI_ASSOC);
+$mricnt = "";
+foreach ($mrirows as $row) {
+  $mricnt .= implode(",", $row) . "\n";
 }
 $updatemricnt = "UPDATE reports SET count=$mricnt WHERE id = 6 ";
 
 
 // Job Updation 
 
-  $jobcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('job', query) > 0 ";
-  $job = mysqli_query($conn, $jobcount);
-  $jobrows = mysqli_fetch_all($job, MYSQLI_ASSOC);
-  $jobcnt = "";
-foreach($jobrows as $row) {
-    $jobcnt .= implode(",", $row) . "\n";
+$jobcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('job', query) > 0 ";
+$job = mysqli_query($conn, $jobcount);
+$jobrows = mysqli_fetch_all($job, MYSQLI_ASSOC);
+$jobcnt = "";
+foreach ($jobrows as $row) {
+  $jobcnt .= implode(",", $row) . "\n";
 }
 $updatejobcnt = "UPDATE reports SET count=$jobcnt WHERE id = 7 ";
 
 
 // Transfer Updation 
 
-  $transcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('transfer', query) > 0 ";
-  $trans = mysqli_query($conn, $transcount);
-  $transrows = mysqli_fetch_all($trans, MYSQLI_ASSOC);
-  $transcnt = "";
-foreach($transrows as $row) {
-    $transcnt .= implode(",", $row) . "\n";
+$transcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('transfer', query) > 0 ";
+$trans = mysqli_query($conn, $transcount);
+$transrows = mysqli_fetch_all($trans, MYSQLI_ASSOC);
+$transcnt = "";
+foreach ($transrows as $row) {
+  $transcnt .= implode(",", $row) . "\n";
 }
 $updatetranscnt = "UPDATE reports SET count=$transcnt WHERE id = 8 ";
 
 
 // Promotion Updation 
 
-  $procount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('promotion', query) > 0 ";
-  $pro = mysqli_query($conn, $procount);
-  $prorows = mysqli_fetch_all($pro, MYSQLI_ASSOC);
-  $procnt = "";
-foreach($prorows as $row) {
-    $procnt .= implode(",", $row) . "\n";
+$procount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('promotion', query) > 0 ";
+$pro = mysqli_query($conn, $procount);
+$prorows = mysqli_fetch_all($pro, MYSQLI_ASSOC);
+$procnt = "";
+foreach ($prorows as $row) {
+  $procnt .= implode(",", $row) . "\n";
 }
 $updateprocnt = "UPDATE reports SET count=$procnt WHERE id = 9 ";
 
 
 // Pregnancy Updation 
 
-  $precount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('pregnancy', query) > 0 ";
-  $pre = mysqli_query($conn, $precount);
-  $prerows = mysqli_fetch_all($pre, MYSQLI_ASSOC);
-  $precnt = "";
-foreach($prerows as $row) {
-    $precnt .= implode(",", $row) . "\n";
+$precount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('pregnancy', query) > 0 ";
+$pre = mysqli_query($conn, $precount);
+$prerows = mysqli_fetch_all($pre, MYSQLI_ASSOC);
+$precnt = "";
+foreach ($prerows as $row) {
+  $precnt .= implode(",", $row) . "\n";
 }
 $updateprecnt = "UPDATE reports SET count=$precnt WHERE id = 10 ";
 
 
 // Litigation Updation 
 
-  $precount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('litigation', query) > 0 ";
-  $pre = mysqli_query($conn, $precount);
-  $prerows = mysqli_fetch_all($pre, MYSQLI_ASSOC);
-  $precnt = "";
-foreach($prerows as $row) {
-    $precnt .= implode(",", $row) . "\n";
+$precount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('litigation', query) > 0 ";
+$pre = mysqli_query($conn, $precount);
+$prerows = mysqli_fetch_all($pre, MYSQLI_ASSOC);
+$precnt = "";
+foreach ($prerows as $row) {
+  $precnt .= implode(",", $row) . "\n";
 }
 $updateprecnt = "UPDATE reports SET count=$precnt WHERE id = 11 ";
 
@@ -162,7 +162,7 @@ $forgcount = "SELECT COUNT(query) FROM appointment WHERE FIND_IN_SET('foreigntra
 $forg = mysqli_query($conn, $forgcount);
 $forgrows = mysqli_fetch_all($forg, MYSQLI_ASSOC);
 $forgcnt = "";
-foreach($forgrows as $row) {
+foreach ($forgrows as $row) {
   $forgcnt .= implode(",", $row) . "\n";
 }
 $updateforgcnt = "UPDATE reports SET count=$forgcnt WHERE id = 12 ";
@@ -183,30 +183,43 @@ $updateforgcnt = "UPDATE reports SET count=$forgcnt WHERE id = 12 ";
 
 // Queries Bar Chart
 
-$query2 = "SELECT * FROM reports";
-$result2 = mysqli_query($conn, $query2);
-$data2 = array();
+
+// Google Query Bar Chart
+
+// $query2 = "SELECT * FROM reports";
+// $result2 = mysqli_query($conn, $query2);
+// $data2 = array();
 
 
-if (mysqli_num_rows($result2) > 0) {
-    while ($row = mysqli_fetch_assoc($result2)) {
-        $data2[] = $row;
-    }
+// if (mysqli_num_rows($result2) > 0) {
+//   while ($row = mysqli_fetch_assoc($result2)) {
+//     $data2[] = $row;
+//   }
+// }
+
+// // print_r($data2);
+
+// $result2 = [];
+// foreach ($data2 as $d) {
+//   $result2[] = array($d['query'], (int) $d['count']);
+// }
+// // print_r($result2);
+// $header = array("query", "count");
+// $result2 = array_merge(array($header), $result2);
+
+// $querytable = json_encode($result2);
+
+
+// Canvas Query Bar Chart 
+
+$test=array();
+$count=0;
+$res=mysqli_query($conn,"SELECT * FROM reports");
+while($row=mysqli_fetch_array($res)){
+$test[$count]['label']=$row['query'];
+$test[$count]['y']=$row['count'];
+$count=$count+1;
 }
-
-// print_r($data2);
-
-$result2 = [];
-foreach ($data2 as $d) {
-  $result2[] = array($d['query'], (int)$d['count']);
-}
-// print_r($result2);
-$header = array("query", "count");
-$result2 = array_merge(array($header), $result2);
-
-$querytable = json_encode($result2);
-
-
 
 
 ?>
@@ -216,103 +229,101 @@ $querytable = json_encode($result2);
 <head>
 
 
-    <!-- Gender Pie Chart  -->
+  <!-- Gender Pie Chart  -->
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        google.charts.load('current', { 'packages': ['corechart'] });
-        google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-
-            var data = google.visualization.arrayToDataTable([
-                ['gender', 'Number'],
-                <?php
-                while ($row = mysqli_fetch_array($result1)) {
-                    echo "['" . $row["gender"] . "', " . $row["number"] . "],";
-                }
-                ?>
-            ]);
-
-            var options = {
-                title: 'Gender Ratio'
-            };
-
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-            chart.draw(data, options);
-        }
-    </script>
-
-
-
-    <!-- Queries Bar Chart   -->
-
-    <!-- <script type="text/javascript">
-    google.charts.load("current", {packages:['corechart']});
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load('current', { 'packages': ['corechart'] });
     google.charts.setOnLoadCallback(drawChart);
+
     function drawChart() {
+
       var data = google.visualization.arrayToDataTable([
-        ["Element", "Density", { role: "style" } ],
-        ["Copper", 8.94, "#b87333"],
-        ["Silver", 10.49, "silver"],
-        ["Gold", 19.30, "gold"],
-        ["Platinum", 21.45, "color: #e5e4e2"]
+        ['gender', 'Number'],
+        <?php
+        while ($row = mysqli_fetch_array($result1)) {
+          echo "['" . $row["gender"] . "', " . $row["number"] . "],";
+        }
+        ?>
       ]);
 
+      var options = {
+        title: 'Gender Ratio'
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+      chart.draw(data, options);
+    }
+  </script>
+
+
+
+  <!-- Queries Bar Chart   -->
+
+
+  <!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+
+      var data = google.visualization.arrayToDataTable(<?php echo $querytable; ?>);
+
       var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
+
+      // view.setColumns([0, 1,
+      //     {
+      //         calc: "stringify",
+      //         sourceColumn: 1,
+      //         type: "string",
+      //         role: "annotation"
+      //     },
+      //     2]);
 
       var options = {
-        title: "Density of Precious Metals, in g/cm^3",
-        width: 600,
+        title: "Query Types",
+        width: 1500,
         height: 400,
-        bar: {groupWidth: "95%"},
+        bar: { groupWidth: "55%" },
         legend: { position: "none" },
+        // colors: ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099','#3366CC', '#DC3912', '#FF9900', '#109618', '#990099','#109618', '#990099'],
+
+        colors: ['#66008d', '#bd21f9', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6', '#f7d9c4', '#fae5d3', '#fbebe4', '#fcf2f0', '#fcf8f7', '#fcfbfb'],
+        // colors: colorPalate,
+        // colors: ['green'],
+        // colorPalate : 
+        // is3D: true,
+        // colorByPoint: true
       };
       var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
       chart.draw(view, options);
-  }
+    }
   </script> -->
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        google.charts.load("current", { packages: ['corechart'] });
-        google.charts.setOnLoadCallback(drawChart);
-        function drawChart() {
 
-            var data = google.visualization.arrayToDataTable(<?php echo $querytable; ?>);
+  <script>
+    window.onload = function () {
 
-            var view = new google.visualization.DataView(data);
+      var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        theme: "light1",
+        title: {
+          text: "Query Reports"
+        },
+        axisY: {
+          title: "Number of Queries"
+        },
+        data: [{
+          type: "column",
+          yValueFormatString: "#,##0.## tonnes",
+          dataPoints: <?php echo json_encode($test , JSON_NUMERIC_CHECK); ?>
+        }]
+      });
+      chart.render();
 
-            // view.setColumns([0, 1,
-            //     {
-            //         calc: "stringify",
-            //         sourceColumn: 1,
-            //         type: "string",
-            //         role: "annotation"
-            //     },
-            //     2]);
-
-            var options = {
-                title: "Query Types",
-                width: 1500,
-                height: 400,
-                bar: { groupWidth: "55%" },
-                legend: { position: "none" },
-            };
-            var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-            chart.draw(view, options);
-        }
-    </script>
-
-
-
+    }
+  </script>
 
 
 
@@ -321,11 +332,14 @@ $querytable = json_encode($result2);
 
 <body>
 
-    <h1>Appointment Reports</h1>
+  <h1>Appointment Reports</h1>
 
 
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-    <div id="columnchart_values" style="width: 900px; height: 500px;"></div>
+  <div id="piechart" style="width: 900px; height: 500px;"></div>
+
+  <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+  <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
 
 
 </body>
