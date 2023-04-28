16a2bf70b61d3c69fe8e10296d1bf69edb6041b6
Ganesh <wakadeganesh6@gmail.com>
2023-04-28 18:49:41 +0530

    student data updation page

---
+++ update_design.php
@@ -0,0 +1,444 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "astrology");
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
+$id = $_GET['id'];
+$sql = "SELECT * FROM coursebooking WHERE id = '$id'";
+$result = mysqli_query($mysqli, $sql);
+$row = mysqli_fetch_assoc($result);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    body {
+      padding-top: 4.5rem;
+    }
+
+    .main-page-container {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-top: 55px;
+    }
+
+    .heading {
+      font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+      font-size: 10px;
+      font-weight: bold;
+      text-align: center;
+    }
+
+    .orange-container {
+      z-index: -10;
+      left: 50px;
+      position: absolute;
+      background-color: peru;
+      height: 800px;
+      width: 15%;
+      border-radius: 20px;
+    }
+
+    .floating-cushion {
+      height: 110%;
+      background-color: #f2f2f2;
+      border-radius: 20px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      padding: 20px;
+      position: relative;
+      width: 70%;
+    }
+
+    .incoushin {
+      padding-left: 150px;
+      padding-right: 100px;
+    }
+
+    .heading {
+      font-size: 2.5rem;
+      padding-bottom: 1.5rem;
+    }
+
+    form {
+      display: grid;
+      grid-template-rows: repeat(3, 3fr);
+      gap: 10px;
+    }
+
+    .form-row {
+      display: grid;
+      grid-template-columns: repeat(3, 3fr);
+      gap: 15px;
+    }
+
+    input,
+    select {
+      border: none;
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 16px;
+      outline: none;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    }
+
+    button {
+      border: none;
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 24px;
+      outline: none;
+      background-color: peru;
+      color: white;
+      cursor: pointer;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+      transform: translate(290%, 100%);
+      margin: 0 auto;
+      display: block;
+      width: 240px;
+      height: 60px;
+    }
+
+    .dropdown-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+    }
+
+    #myDropdown option[value="0"] {
+      color: green;
+    }
+
+    #myDropdown option[value="visitor"] {
+      color: red;
+    }
+
+    /* Style for the "Courier" option */
+    #myDropdown option[value="courier"] {
+      color: blue;
+    }
+
+    button:hover {
+      background-color: peru;
+    }
+
+    /* Media queries for responsiveness */
+
+    @media screen and (max-width: 768px) {
+      .floating-cushion {
+        width: 90%;
+      }
+
+      .form-row {
+        grid-template-columns: repeat(2, 1fr);
+      }
+    }
+
+    @media screen and (max-width: 480px) {
+      .floating-cushion {
+        width: 100%;
+        padding: 10px;
+      }
+
+      .form-row {
+        grid-template-columns: 1fr;
+      }
+
+      input,
+      select {
+        font-size: 14px;
+        padding: 8px;
+      }
+
+      button {
+        font-size: 14px;
+        padding: 8px;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <div class="main-page-container">
+    <div class="orange-container">
+
+    </div>
+    <div class="floating-cushion">
+      <div class="incoushin">
+        <div class="heading">Update Student Details</div>
+
+        <form action="#" method="post">
+          <div class="dropdown-container">
+            <select name="type" placeholder="Type" id="myDropdown">
+              <option value="0">Type</option>
+              <option value="visitor" id="visitor">Query</option>
+              <option value="courier" id="courier">Book Course</option>
+            </select>
+          </div>
+          <div>
+            <input type="text" name="first-name" value="<?php echo $row['first_name']; ?>" placeholder="First Name"
+              class="courier">
+            <input type="text" name="middle-name" value="<?php echo $row['middle_name']; ?>" placeholder="Middle Name"
+              class="courier">
+            <input type="text" name="last-name" value="<?php echo $row['last_name']; ?>" placeholder="Last Name"
+              class="courier">
+            <input type="text" name="fathers-name" value="<?php echo $row['fathers_name']; ?>"
+              placeholder="Fathers Name" class="courier">
+          </div>
+          <div>
+            <input type="text" name="phone-number" value="<?php echo $row['mob_no']; ?>" placeholder="Phone-number"
+              class="courier">
+            <input type="text" name="email" value="<?php echo $row['email_id']; ?>" placeholder="Email" class="courier">
+            <input type="text" name="address" value="<?php echo $row['address']; ?>" placeholder="Address"
+              class="courier">
+            <input type="text" name="pin_code" value="<?php echo $row['pin_code']; ?>" placeholder="PinCode"
+              class="courier">
+          </div>
+          <div class="form-row">
+            <select class="visitor" name="coursename" placeholder="Select Course Name">
+              <option value="0">Select Course Name</option>
+              <option value="tarot" <?php
+              if ($row['course_name'] == 'tarot') {
+                echo "selected";
+              }
+              ?>>Tarot Cards Course
+              </option>
+              <option value="chaledean" <?php
+              if ($row['course_name'] == 'chaledean') {
+                echo "selected";
+              }
+              ?>>Chaledean
+                Numerology Course</option>
+              <option value="lalkitaab" <?php
+              if ($row['course_name'] == 'lalkitaab') {
+                echo "selected";
+              }
+              ?>>Lal Kitaab
+                Course</option>
+              <option value="vastu" <?php
+              if ($row['course_name'] == 'vastu') {
+                echo "selected";
+              }
+              ?>>Vastu Shastra Course
+              </option>
+              <option value="vedic" <?php
+              if ($row['course_name'] == 'vedic') {
+                echo "selected";
+              }
+              ?>>Vedic Astrology Course
+              </option>
+              <option value="mobilecourse" <?php
+              if ($row['course_name'] == 'mobilecourse') {
+                echo "selected";
+              }
+              ?>>Mobile
+                Numerology Course</option>
+            </select>
+            <select name="coursetype" placeholder="Select Course Name" class="details">
+              <option value="Course Type">Course Type</option>
+              <option value="envelope" <?php
+              if ($row['course_type'] == 'envelope') {
+                echo "selected";
+              }
+              ?>>Basic</option>
+              <option value="box" <?php
+              if ($row['course_type'] == 'box') {
+                echo "selected";
+              }
+              ?>>Advance</option>
+            </select>
+            <select name="Batch" placeholder="Please Course First" class="details">
+              <option value="Courier">Batch</option>
+              <option value="april" <?php
+              if ($row['batch'] == 'april') {
+                echo "selected";
+              }
+              ?>>April</option>
+              <option value="may" <?php
+              if ($row['batch'] == 'may') {
+                echo "selected";
+              }
+              ?>>May</option>
+            </select>
+          </div>
+          <div class="fee">
+            <input type="text" value="<?php echo $row['fee']; ?>" placeholder="total fees" name="fee">
+          </div>
+          <div class="form-row">
+            <select name="pay_type" placeholder="Select Payment Method">
+              <option value="Payment Type">Payment Type</option>
+              <option value="fullpayment" <?php
+              if ($row['pay_type'] == 'fullpayment') {
+                echo "selected";
+              }
+              ?>>Full Payment
+              </option>
+              <option value="emi" <?php
+              if ($row['pay_type'] == 'emi') {
+                echo "selected";
+              }
+              ?>>EMI</option>
+            </select>
+            <select name="upi" placeholder="Select Payment Method">
+              <option value="Select Payment Method">Select Payment Method</option>
+              <option value="upi" <?php
+              if ($row['upi'] == 'upi') {
+                echo "selected";
+              }
+              ?>>UPI</option>
+              <option value="gpay" <?php
+              if ($row['upi'] == 'gpay') {
+                echo "selected";
+              }
+              ?>>GPay</option>
+              <option value="phonepay" <?php
+              if ($row['upi'] == 'phonepay') {
+                echo "selected";
+              }
+              ?>>PhonePay</option>
+              <option value="paytm" <?php
+              if ($row['upi'] == 'paytm') {
+                echo "selected";
+              }
+              ?>>Paytm</option>
+            </select>
+          </div>
+          <div>
+            <input type="text" value="<?php echo $row['upi_no']; ?>" name="UPINumber" placeholder="UPI Number"
+              class="courier">
+            <input type="text" value="<?php echo $row['amount_paid']; ?>" name="AmountPaid" placeholder="Amount Paid"
+              class="courier">
+          </div>
+          <div class="form-row">
+            <span class="details">Transction Date</span>
+            <input value="<?php echo $row['trans_date']; ?>" value="<?php echo $row['trans_date']; ?>" type="date"
+              name="trans_date" required>
+          </div>
+          <div class="form-row">
+            <span class="details">Transction Time</span>
+            <input value="<?php echo $row['trans_time']; ?>" type="time" name="trans_time" required>
+          </div>
+          <div class="form-row">
+            <!-- class name is changed from input-box to input-boxt  -->
+            <span class="details">Transaction</span>
+            <input class="choose" value="<?php echo $row['trans_slip']; ?>" name="trans_slip" type="file"
+              accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps">
+          </div>
+
+          <div class="form-row">
+            <div class="subheading">
+              <span class="slotheading" value="<?php echo $row['remark']; ?>"><u>Remarks</u></span>
+            </div>
+            <div class="form-row">
+              <textarea name="remark" id="" cols="172" rows="4" placeholder="Your Message in 50 words..."></textarea>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-row">
+    <button type="submit" name="register" value="Register">Submit</button>
+  </div>
+  </form>
+  <script>
+
+    const dropdown = document.getElementById("myDropdown");
+    const courierFields = document.querySelectorAll(".courier");
+    const visitorFields = document.querySelectorAll(".visitor");
+
+    // Listen for changes in the dropdown
+    dropdown.addEventListener("change", () => {
+      if (dropdown.value === "visitor") {
+        // Disable courier fields if visitor is selected
+        courierFields.forEach(field => {
+          field.disabled = true;
+        });
+        // Enable visitor fields if visitor is selected
+        visitorFields.forEach(field => {
+          field.disabled = false;
+        });
+      } else if (dropdown.value === "courier") {
+        // Disable visitor fields if courier is selected
+        visitorFields.forEach(field => {
+          field.disabled = true;
+        });
+        // Enable courier fields if courier is selected
+        courierFields.forEach(field => {
+          field.disabled = false;
+        });
+      }
+    });
+  </script>
+</body>
+
+</html>
+<br>
+<br>
+<br>
+<?php
+$mysqli = new mysqli("localhost", "root", "", "astrology");
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
+if ($_POST['register']) {
+
+  $first_name = $_POST['first-name'];
+  $middle_name = $_POST['middle-name'];
+  $last_name = $_POST['last-name'];
+  $father_name = $_POST['fathers-name'];
+  $mob_no = $_POST['phone-number'];
+  $email_id = $_POST['email'];
+  $address = $_POST['address'];
+  $pincode = $_POST['pin_code'];
+  $course_name = $_POST['coursename'];
+  $course_type = $_POST['coursetype'];
+  $batch = $_POST['Batch'];
+  $fee = $_POST['fee'];
+  $pay_type = $_POST['pay_type'];
+  $upi = $_POST['upi'];
+  $upi_no = $_POST['UPINumber'];
+  $amount_paid = $_POST['AmountPaid'];
+  $trans_date = $_POST['trans_date'];
+  $trans_time = $_POST['trans_time'];
+  $trans_slip = $_POST['trans_slip'];
+  $remark = $_POST['remark'];
+  $cer = "Not issued";
+  $id = strtoupper(substr($first_name, 0, 1)) . strtoupper(substr($middle_name, 0, 1)) .
+    strtoupper(substr($last_name, 0, 1)) . strtoupper(substr($course_name, 0, 1)) .
+    strtoupper(substr($course_type, 0, 1)) . strtoupper(substr($batch, 0, 1));
+
+  $query = "UPDATE coursebooking set id='$id' , first_name = '$first_name',middle_name ='$middle_name',last_name='$last_name',
+fathers_name='$father_name',mob_no = '$mob_no',email_id='$email_id', address='$address',pin_code='$pincode',course_name='$course_name',
+course_type='$course_type',batch = '$batch',fee='$fee',pay_type='$pay_type', upi='$upi', upi_no='$upi_no' ,amount_paid='$amount_paid',
+trans_date='$trans_date' ,trans_time='$trans_time',trans_slip='$trans_slip',certificate='$cer',remark='$remark'";
+  $data = mysqli_query($mysqli, $query);
+  if($data){
+ echo "<script>alert('Recorde Update Sucefully');</script>";
+}
+else{
+  echo "<script>alert('connection failed');</script>";
+}
+
+  $sql = "SELECT id FROM coursebooking WHERE first_name = '$first_name' AND last_name='$last_name'";
+  $result = mysqli_query($mysqli, $sql);
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $row["id"];
+    }
+
+  } else {
+    echo "No result found";
+  }
+
+}
+
+?>
